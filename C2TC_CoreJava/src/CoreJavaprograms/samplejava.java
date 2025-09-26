98d006116088bb465e02dc1bb17dc15d65cbb54c
sriram-0509 <asriram545@gmail.com>
2025-09-26 16:21:02 +0530

    I added sample class

---
+++ C2TC_CoreJava/src/CoreJavaprograms/samplejava.java
@@ -0,0 +1,10 @@
+package CoreJavaprograms;
+
+public class samplejava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("welcome to java");
+	}
+
+}
