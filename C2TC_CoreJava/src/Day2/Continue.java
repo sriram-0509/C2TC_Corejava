82b009c746f1fbd3eab2c165619458ab2a6c8435
sriram-0509 <asriram545@gmail.com>
2025-10-03 19:39:02 +0530

    Day2

---
+++ C2TC_CoreJava/src/Day2/Continue.java
@@ -0,0 +1,14 @@
+package Day2;
+
+public class Continue {
+	public static void main(String[] args) {
+		for (int k = 5; k < 15; k++)
+		{
+		if (k%2 != 0)
+		continue;
+		System.out.print(k + " ");
+		}
+	}
+
+}
+
---
+++ C2TC_CoreJava/src/Day2/While.java
@@ -0,0 +1,13 @@
+package Day2;
+
+public class While {
+	public static void main(String[] args) 
+	{
+		int i=99;
+		while(i<=100)
+		{
+			System.out.println(i);
+			i++;
+		}
+	}
+}
