1d336999b86e6b9c0dd2fbe7dc8a0cbc05bff9f5
sriram-0509 <asriram545@gmail.com>
2025-10-07 15:34:56 +0530

    day four updated

---
+++ C2TC_CoreJava/src/com/tnsif/day4/thirdpackage/PrivateConstructor.java
@@ -0,0 +1,16 @@
+package com.tnsif.day4.thirdpackage;
+
+public class PrivateConstructor{
+
+	public static void main(String[] args) {
+		myclass m=myclass.getObject();
+		m.setId(10);
+		
+		myclass m1=myclass.getObject();
+		
+		System.out.println(m);
+		System.out.println(m1);
+
+	}
+
+}
